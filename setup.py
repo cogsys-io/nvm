5bafeb8d1233fa863e2afb10ada4c9f77d82edca
jiko <jiko@stardust7>
2023-05-06 01:17:41 +0200

    upd: only py38 for now

--- setup.py
+++ setup.py
@@ -72,9 +72,9 @@ setup(
         "Natural Language :: English",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3.11",
+        # "Programming Language :: Python :: 3.9",
+        # "Programming Language :: Python :: 3.10",
+        # "Programming Language :: Python :: 3.11",
     ],
     description="Plenty little helpers.",
     entry_points={
