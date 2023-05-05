be3adf25f2a4bd0cdde76478ab76ca7d6d7d2a78
jiko <jiko@stardust7>
2023-05-05 17:20:29 +0200

    mov: CSM → aux_str/clean_str_mappings.py

--- nvm/aux_str/__init__.py
+++ nvm/aux_str/__init__.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 
 from .aux_str import clean_str
-from .aux_str import CLEAN_STR_MAPPINGS_TINY
-from .aux_str import CLEAN_STR_MAPPINGS_LARGE
+from .clean_str_mappings import (
+    CLEAN_STR_MAPPINGS_TINY,
+    CLEAN_STR_MAPPINGS_LARGE,
+    CLEAN_STR_MAPPINGS_HUGE,
+)
