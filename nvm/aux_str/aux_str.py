7da2348852926a67b9dba774b4229682cead0ed6
jiko <jiko@stardust7>
2023-05-19 23:13:38 +0200

    add: is_ascii in aux_str

--- nvm/aux_str/aux_str.py
+++ nvm/aux_str/aux_str.py
@@ -16,6 +16,16 @@ from .clean_str_mappings import (  # noqa: F401
 )
 
 
+def is_ascii(s):
+    """Check if the characters in string s are in ASCII."""
+    return all(ord(c) < 128 for c in s)
+
+
+def is_ascii_alt(s):
+    """Check if the characters in string s are in ASCII, U+0-U+7F."""
+    return len(s) == len(s.encode())
+
+
 def clean_str(
     text: str,
     mappings: List[Dict[str, List[Union[str, Pattern[str]]]]] = CLEAN_STR_MAPPINGS_TINY,
