c6c742c9c6ee6bb5c24bbc521011642219ea5cfd
cogsys-io <113848740+cogsys-io@users.noreply.github.com>
2023-05-20 06:51:50 +0200

    upd: reorder clean_str_mappings import in aux_str

--- nvm/aux_str/__init__.py
+++ nvm/aux_str/__init__.py
@@ -6,11 +6,11 @@ from .aux_str import is_ascii_alt
 from .aux_str import clean_str
 
 from .clean_str_mappings import (
-    CLEAN_STR_MAPPINGS_DROP_HASHTAGS,
     CLEAN_STR_MAPPINGS_TINY,
     CLEAN_STR_MAPPINGS_LARGE,
     CLEAN_STR_MAPPINGS_HUGE,
     CLEAN_STR_MAPPINGS_SPACE,
+    CLEAN_STR_MAPPINGS_DROP_HASHTAGS,
 )
 
 from .regex import (
